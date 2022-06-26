9ec83d394d32291478bea33b83224de536f85a0a
Matias <matiasquiroga.04@gmail.com>
2022-06-25 22:45:09 -0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 const burguer =document.querySelector('.burguer');
 const navbar = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar a');
 
 document.addEventListener('DOMContentLoaded',()=>{
-    mostrarMenu();   
+    showMenu();   
+    closeMenu();
 });
+
 function showMenu(){
     burguer.addEventListener('click',()=>{
 navbar.classList.toggle('hide');
 
     })
+
+function closeMenu(){
+    links.forEach(links =>{
+        links.addEventListener('click', (e)=>{
+
+            if(e.target.tagName === 'A'){
+                navbar.classList.add('hide');
+            }
+        })
+    })
+}
 }
