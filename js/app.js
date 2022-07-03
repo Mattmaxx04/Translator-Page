2ad9603ed2d6d99f7205b9e9acf4c185751b8c98
Matias <matiasquiroga.04@gmail.com>
2022-07-03 17:07:12 -0300

    App.js
    Const Fecha
    Const Section
    Update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+/*menu variables*/
 const burguer =document.querySelector('.burguer');
 const navbar = document.querySelector('.navbar');
 const links = document.querySelectorAll('.navbar a');
+/*menu variables*/
 
+/*date variable*/
+const fecha = document.querySelector('.fecha');
+/*date variable*/
+
+/*menu function*/
 document.addEventListener('DOMContentLoaded',()=>{
     showMenu();   
     closeMenu();
+    fechaActual();
 });
 
 function showMenu(){
@@ -12,15 +20,37 @@ function showMenu(){
 navbar.classList.toggle('hide');
 
     })
-
+}
 function closeMenu(){
     links.forEach(links =>{
         links.addEventListener('click', (e)=>{
-
+            /*smooth behavior*/
+            e.preventDefault();
+            const section = document.querySelector(e.target.attributes.href.value);
+            changeSection(section);
+            /*smooth behavior*/
             if(e.target.tagName === 'A'){
                 navbar.classList.add('hide');
             }
         })
     })
 }
+
+/*menu function*/
+
+/*smooth behavior*/
+function changeSection(section){
+    section.scrollIntoView({
+        behavior:'smooth'
+    })
 }
+/*smooth behavior*/
+
+/*date function*/
+function fechaActual(){
+    let fechaHoy = new Date().getFullYear();
+    fecha.textContent = fechaHoy;
+}
+
+
+/*date function*/
